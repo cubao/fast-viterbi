8eca64172191abfe4ca649fb2b78a172b4791067
TANG ZHIXIONG <zhixiong.tang@momenta.ai>
2024-05-18 10:47:57 +0800

    not ready

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <limits>
 #include <map>
 #include <vector>
+#include <unordered_map>
 
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
@@ -19,7 +20,7 @@ struct FastViterbi {
     using NodeIndex = std::tuple<LayerIndex, CandidateIndex>;
     FastViterbi(int K, int N, const std::map<std::tuple<NodeIndex, NodeIndex>, double> &scores) : K_(K), N_(N) {
         if (K == 0 || N < 2) {
-            return;
+            throw std::invalid_argument("invalid K, N = " + std::to_string(K) + ", " + std::to_string(N));
         }
         links_ = std::vector<std::vector<Links>>(N - 1, std::vector<Links>(K));
         for (auto &pair : scores) {
@@ -33,6 +34,7 @@ struct FastViterbi {
             if (lidx0 < 0) {
                 if (lidx1 == 0 && cidx1 < K) {
                     heads_.push_back({cidx1, score});
+                    scores_[-1][-1][cidx1] = score;
                 }
                 continue;
             }
@@ -43,10 +45,29 @@ struct FastViterbi {
                 continue;
             }
             links_[lidx0][cidx0].push_back({cidx1, score});
+            scores_[lidx0][cidx0][cidx1] = score;
         }
     }
 
-    std::vector<int> inference() const { return {}; }
+    std::vector<double> scores(const std::vector<int> &node_path) const {
+        if (node_path.size() != N_) {
+            return {};
+        }
+        std::vector<double> ret;
+        ret.reserve(N_);
+        double acc = scores_.at(-1).at(-1).at(node_path[0]);
+        ret.push_back(acc);
+        for (int n = 0; n < N_ - 1; ++n) {
+            acc += scores_.at(n).at(node_path[n]).at(node_path[n+1]);
+            ret.push_back(acc);
+        }
+        return ret;
+    }
+
+    std::vector<int> inference() const {
+        // forward
+        // backward
+    }
 
     bool setup_roads(const std::vector<std::vector<int64_t>> &roads) {
         if (roads.size() != N_) {
@@ -58,6 +79,7 @@ struct FastViterbi {
             int K = roads[n].size();
             if (K > K_) {
                 roads_.clear();
+                std::cerr << "invalid road ids at #layer=" << n << ", #candidates=" << K << std::endl;
                 return false;
             }
             for (int k = 0; k < K; ++k) {
@@ -67,15 +89,41 @@ struct FastViterbi {
         return true;
     }
 
+    std::vector<int64_t> road_path(const std::vector<int> &node_path) const {
+        if (node_path.empty()) {
+            std::cerr << "empty node path!" << std::endl;
+            return {};
+        }
+        if (roads_.empty()) {
+            std::cerr << "roads not inited!" << std::endl;
+            return {};
+        }
+        std::vector<int64_t> path;
+        path.push_back(roads_[0][node_path[0]]);
+        for (int n = 1; n < node_path.size(); ++n) {
+            auto r = roads_[0][node_path[0]];
+            if (r == path.back()) {
+                continue;
+            }
+            path.push_back(r);
+        }
+        return path;
+    }
+
     std::vector<int> inference(const std::vector<int> &path) const { return {}; }
 
   private:
     const int K_{-1};
     const int N_{-1};
     using Links = std::vector<std::pair<int, double>>;
+    // head layer: cidx, score
     Links heads_;
+    // tail layers, [[cidx (in next layer), score]]
     std::vector<std::vector<Links>> links_;
+    // road ids, K * N
     std::vector<std::vector<int64_t>> roads_;
+    // score map, lidx -> cidx -> next_cidx -> score
+    std::unordered_map<int, std::unordered_map<int, std::unordered_map<int, double>>> scores_;
 };
 }  // namespace cubao
 
