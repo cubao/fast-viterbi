1da69f49de1bfad699c60216b9a22ae995215103
TANG ZhiXiong <district10@users.noreply.github.com>
2024-05-18 20:13:58 +0800

    tested okay (#3)
    * fix test
    
    * init
    
    * not ready
    
    * not ready
    
    * not ready
    
    * ready
    
    * not ready
    
    * not ready
    
    * good
    
    * not ready
    
    * not ready
    
    * ready
    
    * update
    
    * not ready
    
    * not ready
    
    * update
    
    * gcc5.4 compatible
    
    * gil
    
    * good
    
    * good
    
    * good
    
    lint code
    
    * revert
    
    * good
    
    ---------
    
    Co-authored-by: TANG ZHIXIONG <zhixiong.tang@momenta.ai>

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 
-import scikit_build_example as m
+import fast_viterbi as m
 
 
 def test_version():
-    assert m.__version__ == "0.0.1"
+    assert m.__version__ == "0.0.2"
 
 
 def test_add():
